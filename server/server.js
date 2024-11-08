12d2959023c679b2790ae10f12f0137268102e4f
mahoora0 <mahoora022@gmail.com>
2024-11-08 15:06:07 +0900

    .

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: `http://116.124.191.174:15025`,  // 프론트엔드 주소
+  credentials: true
+}));
 app.use(express.json());
 
 // 라우트 추가

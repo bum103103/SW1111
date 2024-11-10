10d9fc4631ea58cc0888ea227fe4bc462cbc48bd
bum103103 <bum103103@naver.com>
2024-11-10 13:30:56 +0900

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+// App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SelectUserPage from './components/SelectUserPage';
 import MyBaeminPage from './components/MyBaeminPage';
 import DeliveryFlow from './components/DeliveryFlow';
 import SettingsPage from './components/SettingsPage';
+import DoorSettingsPage from './components/DoorSettingsPage';
 import { NotificationProvider } from './contexts/NotificationContext';
 
 const App = () => {
@@ -12,20 +14,11 @@ const App = () => {
             <NotificationProvider>
                 <div className="w-full">
                     <Routes>
-                        {/* 초기 선택 화면 */}
                         <Route path="/" element={<SelectUserPage />} />
-                        
-                        {/* 배달원 화면 */}
                         <Route path="/delivery-flow" element={<DeliveryFlow />} />
-                        
-                        {/* 배민 화면 */}
                         <Route path="/baemin" element={<MyBaeminPage />} />
-                        
-                        {/* 설정 페이지 */}
                         <Route path="/settings" element={<SettingsPage />} />
-                        
-                        {/* 도어락 설정 화면 (배민 화면의 설정 아이콘 클릭 시) */}
-                        <Route path="/B/1" element={<DeliveryFlow />} />
+                        <Route path="/doorsettings" element={<DoorSettingsPage />} />
                     </Routes>
                 </div>
             </NotificationProvider>
